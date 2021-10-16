afe34e275993a234d37a138b2ca24d9626ce3a5c
smuktevi <vmuktev1@uw.edu>
2021-10-16 12:52:29 -0700

    added video capture to website

--- routes.py
+++ routes.py
@@ -1,7 +1,18 @@
 from flask.templating import render_template
-from flask import session, render_template, request, redirect, url_for, flash
+from flask import session, request, redirect, url_for, flash, Flask, render_template, render_template_string, Response
 from app import app
 import json
+import cv2
+
+video_capture = cv2.VideoCapture(0)
+
+def gen():    
+    while True:
+        ret, image = video_capture.read()
+        cv2.imwrite('t.jpg', image)
+        yield (b'--frame\r\n'
+           b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
+    video_capture.release()
 
 @app.route("/")
 def homepage():
@@ -11,7 +22,13 @@ def homepage():
 def posture_matching():
     return render_template("trainer.html")
 
-@app.route("/student")
-def posture_tracking():
-    return render_template("student.html")
 
+@app.route('/student')
+def iposture_tracking():
+    """Video streaming"""
+    return render_template('student.html')
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
