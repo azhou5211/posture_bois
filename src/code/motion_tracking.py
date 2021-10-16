c577c3b46c96f252598a9df7182b12f035765313
Andy <azhou5211@gmail.com>
2021-10-15 17:40:29 -0700

    pushing new motion

--- src/code/motion_tracking.py
+++ src/code/motion_tracking.py
@@ -2,7 +2,6 @@ import cv2
 import mediapipe as mp
 import time
 
-
 if __name__ == '__main__':
 
     mpDraw = mp.solutions.drawing_utils
@@ -12,12 +11,14 @@ if __name__ == '__main__':
     cap = cv2.VideoCapture("video_data/1.mp4")
 
     pTime = 0
+    arr = []
     while True:
         success, img = cap.read()
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         results = pose.process(imgRGB)
         # Print the landmark
-        print(results.pose_landmarks)
+        # print(results.pose_landmarks)
+        arr.append(results.pose_landmarks)
 
         # Draw the pose on the img
         if results.pose_landmarks:
