463181c6a0f3b007dfca0490f6dc07b69c4141ef
smuktevi <vmuktev1@uw.edu>
2021-10-16 13:16:44 -0700

    included pose tracker video

--- routes.py
+++ routes.py
@@ -1,17 +1,49 @@
 from flask.templating import render_template
 from flask import session, request, redirect, url_for, flash, Flask, render_template, render_template_string, Response
 from app import app
+# from src.utils.get_data import *
+import mediapipe as mp
 import json
 import cv2
 
-video_capture = cv2.VideoCapture(0)
 
-def gen():    
-    while True:
-        ret, image = video_capture.read()
-        cv2.imwrite('t.jpg', image)
-        yield (b'--frame\r\n'
-           b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
+def track_video():
+
+    mp_drawing = mp.solutions.drawing_utils
+    mp_pose = mp.solutions.pose
+
+    cap = cv2.VideoCapture(0)
+
+    ## Setup mediapipe instance
+    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            
+            # Recolor image to RGB
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+            
+            # Make detection
+            results = pose.process(image)
+
+            # Recolor back to BGR
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            
+            # Render detections
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                    mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                        )               
+            
+            cv2.imwrite('t.jpg', image)
+
+            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            
+    cap.release()
+    cv2.destroyAllWindows()
     video_capture.release()
 
 @app.route("/")
@@ -31,4 +63,4 @@ def iposture_tracking():
 @app.route('/video_feed')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(track_video(), mimetype='multipart/x-mixed-replace; boundary=frame')
